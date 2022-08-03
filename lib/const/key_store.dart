7413517c14fa2527b01ce4679c409b20f6a6ea7d
Ruble <pack.ruble@gmail.com>
2022-08-03 16:44:04 +0300

    add a default key and value store

---
+++ lib/const/key_store.dart
@@ -0,0 +1,92 @@
+///
+/// Простой класс для хранения ключей и значений по умолчанию для хранилища key-value.
+class Store {
+  Store._();
+
+  // Настройки взаимодействия с сервисом погоды.
+  // ===========================================================================
+
+  // enum
+  static const String temperatureUnits = 'temperatureUnits';
+  static const int temperatureUnitsDefault = 1;
+
+  // enum
+  static const String speedUnits = 'speedUnits';
+  static const int speedUnitsDefault = 0;
+
+  // OWM == OpenWeatherMap service
+  static const String userApiKeyOWM = 'userApiKeyOWM';
+  static const String userApiKeyOWMDefault = '';
+
+  static const String userWeatherLanguage = 'userWeatherLanguage';
+  static const String userWeatherLanguageDefault = 'ru';
+
+  static const String lastRequestTimeOneCall = 'lastRequestTimeOneCall';
+  static int lastRequestTimeOneCallDefault = 0;
+
+  static const String lastRequestTimeCurrent = 'lastRequestTimeCurrent';
+  static int lastRequestTimeCurrentDefault = 0;
+
+  // Сохраненные погодные данные.
+  // ===========================================================================
+
+  static const String currentPlace = 'currentPlace';
+  static const String currentPlaceDefault = '';
+
+  static const String savedPlaces = 'savedPlaces';
+  static const List<String> savedPlacesDefault = [];
+
+  static const String weatherOneCall = 'weatherOneCall';
+  static String weatherOneCallDefault = '';
+
+  static const String weatherCurrent = 'weatherCurrent';
+  static String weatherCurrentDefault = '';
+
+  // Настройки приложения.
+  // ===========================================================================
+
+  static const String themeScheme = 'themeScheme';
+  static const int themeSchemeDefault = 0;
+
+  // enum
+  static const String themeMode = 'themeMode';
+  static const int themeModeDefault = 0;
+
+  static const String swapColorsTheme = 'swapColorsTheme';
+  static const bool swapColorsThemeDefault = false;
+
+  static const String swapDarkMainAndContainerColors =
+      'swapDarkMainAndContainerColors';
+  static const bool swapDarkMainAndContainerColorsDefault = false;
+
+  static const String useMaterial3 = 'useMaterial3';
+  static const bool useMaterial3Default = true;
+
+  static const String darkLevelTheme = 'darkLevelTheme';
+  static const int darkLevelThemeDefault = 0;
+
+  static const String darkIsTrueBlack = 'darkIsTrueBlack';
+  static const bool darkIsTrueBlackDefault = false;
+
+  static const String appLocale = 'appLocale';
+  static const String appLocaleDefault = '';
+
+  // enum
+  static const String visualDesignApp = 'visualDesignApp';
+  static const int visualDesignAppDefault = 0;
+
+  static const String textScaleFactor = 'textScaleFactor';
+  static const double textScaleFactorDefault = 1.0;
+
+  // enum
+  static const String typography = 'typography';
+  static const int typographyDefault = 2;
+
+  // enum
+  static const String fontFamily = 'fontFamily';
+  static const int fontFamilyDefault = 0;
+
+  // enum
+  static const String scrollPhysics = 'scrollPhysics';
+  static const int scrollPhysicsDefault = 0;
+}
