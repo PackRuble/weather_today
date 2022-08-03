2dfc162fe9c0f04b27ad386a6e029b7f722d4545
Ruble <pack.ruble@gmail.com>
2022-08-03 17:23:51 +0300

    add const

---
+++ lib/const/app_icons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+/// Класс содержит все иконки приложения
+class AppIcons {
+  AppIcons._();
+
+  // daily page icons
+  static const IconData dailyDirectWind = Icons.navigation_rounded;
+}
---
+++ lib/const/app_insets.dart
@@ -0,0 +1,22 @@
+/// AppInsets represents the sizes used in this app.
+class AppInsets {
+  AppInsets._();
+
+  /// Скругление кнопок
+  static const double cornerRadius = 14;
+
+  /// Толщина рамки кнопок
+  static const double outlineThickness = 1.5;
+
+  /// Высота Floating Search Bar.
+  static const double heightSearchBar = 48.0;
+
+  /// дополнительный отступ сверху для поиск-виджета.
+  static const double aroundPaddingSearchBar = 6.0;
+
+  /// Высота bottom bar.
+  static const double heightBottomBar = 56.0;
+
+  /// Множитель для иконок и картинок.
+  static const double scaleFactorIcon = 0.95;
+}
