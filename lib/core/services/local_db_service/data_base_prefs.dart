d1d5ddd2f36cd352dab993ae9c8fdeb4b0eb4c4a
Ruble <pack.ruble@gmail.com>
2022-08-17 15:24:46 +0300

    bug fixed - saved and loaded correct

--- lib/core/services/local_db_service/data_base_prefs.dart
+++ lib/core/services/local_db_service/data_base_prefs.dart
@@ -14,66 +14,83 @@ class DataBasePrefs with DbLoggy implements IDataBase {
   @override
   Future<void> init() async => _prefs = await SharedPreferences.getInstance();
 
-  /// Загрузка значений из [SharedPreferences] используя key из [Store] с некоторым компромиссом... тодо
-  /// Учитываем, что первоначальные значения
+  /// Загрузка значений из [SharedPreferences] используя key из [Store].
   @override
   Future<T> load<T>(String key, T defaultValue) async {
-    T value;
-
+    Object? value;
     try {
-      value = _prefs.get(key) as T;
+      if (sameTypes<T, List<String>>()) {
+        value = _prefs.getStringList(key) as T;
+      } else {
+        // простые типы не нуждаются в cast
+        // coldfix: добавить проверку других типов, если необходимо
+        value = _prefs.get(key);
+      }
+
+      // значения ещё нет в бд, либо проблемы с cast
+      if (value == null) {
+        loggy.info(
+            'For *$key* value is null, loaded the Default: *$defaultValue*');
+        return defaultValue;
+      }
+
+      loggy.info('LOADED --> key: *$key*, value: *$value* | type value: *$T*');
+      return value as T;
     } catch (e, s) {
-      loggy.warning(
+      loggy.error(
           'Not loaded for key: *$key*! Has been loaded the defaultValue: *$defaultValue* | type value: *$T*',
-          e);
+          e,
+          s);
       return defaultValue;
     }
-
-    loggy.info(
-        'LOADED --> key: *$key*, value: *$value* | type value: *$T* | defaultValue: *$defaultValue*');
-    return value;
   }
 
   /// Save a setting in [SharedPreferences], using `key` as its storage key.
   @override
   Future<void> save<T>(String key, T value) async {
-    try {
-      if (sameTypes<T, bool>()) {
-        await _prefs.setBool(key, value as bool);
-        return;
-      }
+    Future<bool> _save() async {
+      try {
+        if (sameTypes<T, bool>()) {
+          await _prefs.setBool(key, value as bool);
+          return true;
+        }
 
-      if (sameTypes<T, int>()) {
-        await _prefs.setInt(key, value as int);
-        return;
-      }
+        if (sameTypes<T, int>()) {
+          await _prefs.setInt(key, value as int);
+          return true;
+        }
 
-      if (sameTypes<T, double>()) {
-        await _prefs.setDouble(key, value as double);
-        return;
-      }
+        if (sameTypes<T, double>()) {
+          await _prefs.setDouble(key, value as double);
+          return true;
+        }
 
-      if (sameTypes<T, String>()) {
-        await _prefs.setString(key, value as String);
-        return;
-      }
+        if (sameTypes<T, String>()) {
+          await _prefs.setString(key, value as String);
+          return true;
+        }
 
-      if (sameTypes<T, List<String>>()) {
-        await _prefs.setStringList(key, value as List<String>);
-        return;
-      }
+        if (sameTypes<T, List<String>>()) {
+          await _prefs.setStringList(key, value as List<String>);
+          return true;
+        }
 
-      if (value is Enum) {
-        await _prefs.setInt(key, value.index);
-        return;
+        if (value is Enum) {
+          await _prefs.setInt(key, value.index);
+          return true;
+        }
+
+        throw Exception('Wrong type for saving to database');
+      } catch (e, s) {
+        loggy.error(
+            'Not saved --> key: *$key* | value: *$value* | type value: *$T*',
+            e,
+            s);
+        return false;
       }
-    } catch (e, s) {
-      loggy.error(
-          'Not saved --> key: *$key* | value: *$value* | type value: *$T*',
-          e,
-          s);
-      return;
-    } finally {
+    }
+
+    if (await _save()) {
       loggy.info('SAVED --> key: *$key* | value: *$value*, | type value: *$T*');
     }
   }
