7ce5a60a5c1b9dc2ee1238426b23d038c8ff78f5
Ruble <pack.ruble@gmail.com>
2022-09-05 19:09:33 +0300

    add - country flags page
    * add - перевод
    * fix - цветовая палитра + отступы

---
+++ lib/ui/pages/settings_page/country_flags_page/country_flags_page_controller.dart
@@ -0,0 +1,28 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:weather_today/const/countries_code.dart';
+import 'package:weather_today/core/controllers/localization_controller.dart';
+import 'package:weather_today/i18n/translations.g.dart';
+
+/// Class represent controller [CountryFlagsPage].
+class CountryFlagsPageController {
+  CountryFlagsPageController(this._ref);
+
+  final Ref _ref;
+
+  Reader get _reader => _ref.read;
+
+  Reader get _refresh => _ref.refresh;
+
+  static final cr = Provider.autoDispose<CountryFlagsPageController>(
+      (ref) => CountryFlagsPageController(ref));
+
+  /// Провайдер возвращает translate.
+  static final tr = Provider.autoDispose<TranslationsRu>(
+      (ref) => ref.watch(AppLocalize.currentTranslation));
+
+  /// Получить страны вида:
+  /// [[countryCode, countryName], ...]
+  List<List<String>> getCountries() => [
+        for (final e in countriesCode.entries) [e.key, e.value]
+      ];
+}
---
+++ lib/ui/pages/settings_page/country_flags_page/county_flags_page_ui.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:loggy/loggy.dart';
+import 'package:weather_today/const/app_colors.dart';
+import 'package:weather_today/core/services/app_theme_service/controller/app_theme_controller.dart';
+import 'package:weather_today/ui/utils/image_helper.dart';
+
+import '../../../shared/custom_appbar.dart';
+import '../../../shared/wrapper_page.dart';
+import 'country_flags_page_controller.dart';
+
+const double _inset = 8.0;
+
+class CountryFlagsPage extends ConsumerWidget with UiLoggy {
+  const CountryFlagsPage();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    loggy.info('build');
+
+    // [[countryCode, countryName], ...]
+    final List<List<String>> countries =
+        ref.watch(CountryFlagsPageController.cr).getCountries();
+
+    final t = ref.watch(CountryFlagsPageController.tr);
+
+    return WrapperPage(
+      child: Scaffold(
+        appBar: RAppBar(t.flagsPage.appbarTitle),
+        body: GridView.custom(
+          padding: const EdgeInsets.all(_inset),
+          physics: ref.watch(AppTheme.scrollPhysics).scrollPhysics,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1 / 1,
+            crossAxisSpacing: _inset,
+            mainAxisSpacing: _inset,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (_, int index) {
+              return _TileCountryWidget(
+                  countries[index][0], countries[index][1]);
+            },
+            childCount: countries.length,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TileCountryWidget extends ConsumerWidget {
+  const _TileCountryWidget(this.countryCode, this.countryName);
+
+  final String countryCode;
+  final String countryName;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: AppColors.of(context).cardColor,
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: AppColors.of(context).cardBorderColor),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(_inset / 2),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 50.0,
+              child: Center(
+                child: ImageHelper.getFlagIcon(countryCode),
+              ),
+            ),
+            const SizedBox(height: 5.0),
+            Column(
+              children: [
+                Text(countryCode),
+                Text(
+                  countryName,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 5.0),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
