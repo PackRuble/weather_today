ef8bcc0875cc90cd8f1599b582806eab4a977615
Ruble <pack.ruble@gmail.com>
2022-09-24 13:46:18 +0300

    add - translation auto generated

--- lib/i18n/translations_enum.dart
+++ lib/i18n/translations_enum.dart
@@ -1,42 +1,80 @@
 import 'package:open_weather_api/open_weather_api.dart';
 import 'package:weather_today/core/controllers/localization_controller.dart';
+import 'package:weather_today/i18n/translations.g.dart';
+
+import '../ui/pages/main_page/model/homepage_index_model.dart';
+
+extension AppLocaleTr on AppLocale {
+  String get nameTr {
+    switch (this) {
+      case AppLocale.ru:
+        return 'Русский';
+      case AppLocale.en:
+        return 'English';
+    }
+  }
+}
+
+extension HomepageIndexTr on HomepageIndex {
+  String get nameTr {
+    switch (this) {
+      case HomepageIndex.settings:
+        return tr.enums.homepage.settings;
+      case HomepageIndex.hourly:
+        return tr.enums.homepage.hourly;
+      case HomepageIndex.today:
+        return tr.enums.homepage.today;
+      case HomepageIndex.daily:
+        return tr.enums.homepage.daily;
+    }
+  }
+}
 
 extension PressureTr on Pressure {
   String get abbrTr {
     switch (this) {
       case Pressure.hectoPa:
-        return t.units.pressure.abbr.hectoPa;
+        return tr.units.pressure.abbr.hectoPa;
       case Pressure.mbar:
-        return t.units.pressure.abbr.mbar;
+        return tr.units.pressure.abbr.mbar;
       case Pressure.mmHg:
-        return t.units.pressure.abbr.mmHg;
+        return tr.units.pressure.abbr.mmHg;
       case Pressure.kPa:
-        return t.units.pressure.abbr.kPa;
+        return tr.units.pressure.abbr.kPa;
       case Pressure.atm:
-        return t.units.pressure.abbr.atm;
+        return tr.units.pressure.abbr.atm;
       case Pressure.inHg:
-        return t.units.pressure.abbr.inHg;
-      default:
-        return '';
+        return tr.units.pressure.abbr.inHg;
     }
   }
 
   String get nameTr {
     switch (this) {
       case Pressure.hectoPa:
-        return t.units.pressure.name.hectoPa;
+        return tr.units.pressure.name.hectoPa;
       case Pressure.mbar:
-        return t.units.pressure.name.mbar;
+        return tr.units.pressure.name.mbar;
       case Pressure.mmHg:
-        return t.units.pressure.name.mmHg;
+        return tr.units.pressure.name.mmHg;
       case Pressure.kPa:
-        return t.units.pressure.name.kPa;
+        return tr.units.pressure.name.kPa;
       case Pressure.atm:
-        return t.units.pressure.name.atm;
+        return tr.units.pressure.name.atm;
       case Pressure.inHg:
-        return t.units.pressure.name.inHg;
-      default:
-        return '';
+        return tr.units.pressure.name.inHg;
+    }
+  }
+}
+
+extension TempTr on Temp {
+  String get fullNameTr {
+    switch (this) {
+      case Temp.celsius:
+        return tr.units.temp.fullName.celsius;
+      case Temp.kelvin:
+        return tr.units.temp.fullName.kelvin;
+      case Temp.fahrenheit:
+        return tr.units.temp.fullName.fahrenheit;
     }
   }
 }
@@ -45,13 +83,11 @@ extension SpeedTr on Speed {
   String get abbrTr {
     switch (this) {
       case Speed.ms:
-        return t.units.speed.abbr.ms;
+        return tr.units.speed.abbr.ms;
       case Speed.mph:
-        return t.units.speed.abbr.mph;
+        return tr.units.speed.abbr.mph;
       case Speed.kph:
-        return t.units.speed.abbr.kph;
-      default:
-        return '';
+        return tr.units.speed.abbr.kph;
     }
   }
 }
@@ -60,69 +96,63 @@ extension SideOfTheWorldTr on SideOfTheWorld {
   String get abbrTr {
     switch (this) {
       case SideOfTheWorld.north:
-        return t.units.sideOfTheWorld.abbr.north;
+        return tr.units.sideOfTheWorld.abbr.north;
       case SideOfTheWorld.northEast:
-        return t.units.sideOfTheWorld.abbr.northEast;
+        return tr.units.sideOfTheWorld.abbr.northEast;
       case SideOfTheWorld.east:
-        return t.units.sideOfTheWorld.abbr.east;
+        return tr.units.sideOfTheWorld.abbr.east;
       case SideOfTheWorld.southEast:
-        return t.units.sideOfTheWorld.abbr.southEast;
+        return tr.units.sideOfTheWorld.abbr.southEast;
       case SideOfTheWorld.south:
-        return t.units.sideOfTheWorld.abbr.south;
+        return tr.units.sideOfTheWorld.abbr.south;
       case SideOfTheWorld.southWest:
-        return t.units.sideOfTheWorld.abbr.southWest;
+        return tr.units.sideOfTheWorld.abbr.southWest;
       case SideOfTheWorld.west:
-        return t.units.sideOfTheWorld.abbr.west;
+        return tr.units.sideOfTheWorld.abbr.west;
       case SideOfTheWorld.northWest:
-        return t.units.sideOfTheWorld.abbr.northWest;
-      default:
-        return '';
+        return tr.units.sideOfTheWorld.abbr.northWest;
     }
   }
 
   String get nameAdjTr {
     switch (this) {
       case SideOfTheWorld.north:
-        return t.units.sideOfTheWorld.nameAdj.north;
+        return tr.units.sideOfTheWorld.nameAdj.north;
       case SideOfTheWorld.northEast:
-        return t.units.sideOfTheWorld.nameAdj.northEast;
+        return tr.units.sideOfTheWorld.nameAdj.northEast;
       case SideOfTheWorld.east:
-        return t.units.sideOfTheWorld.nameAdj.east;
+        return tr.units.sideOfTheWorld.nameAdj.east;
       case SideOfTheWorld.southEast:
-        return t.units.sideOfTheWorld.nameAdj.southEast;
+        return tr.units.sideOfTheWorld.nameAdj.southEast;
       case SideOfTheWorld.south:
-        return t.units.sideOfTheWorld.nameAdj.south;
+        return tr.units.sideOfTheWorld.nameAdj.south;
       case SideOfTheWorld.southWest:
-        return t.units.sideOfTheWorld.nameAdj.southWest;
+        return tr.units.sideOfTheWorld.nameAdj.southWest;
       case SideOfTheWorld.west:
-        return t.units.sideOfTheWorld.nameAdj.west;
+        return tr.units.sideOfTheWorld.nameAdj.west;
       case SideOfTheWorld.northWest:
-        return t.units.sideOfTheWorld.nameAdj.northWest;
-      default:
-        return '';
+        return tr.units.sideOfTheWorld.nameAdj.northWest;
     }
   }
 
   String get nameTr {
     switch (this) {
       case SideOfTheWorld.north:
-        return t.units.sideOfTheWorld.name.north;
+        return tr.units.sideOfTheWorld.name.north;
       case SideOfTheWorld.northEast:
-        return t.units.sideOfTheWorld.name.northEast;
+        return tr.units.sideOfTheWorld.name.northEast;
       case SideOfTheWorld.east:
-        return t.units.sideOfTheWorld.name.east;
+        return tr.units.sideOfTheWorld.name.east;
       case SideOfTheWorld.southEast:
-        return t.units.sideOfTheWorld.name.southEast;
+        return tr.units.sideOfTheWorld.name.southEast;
       case SideOfTheWorld.south:
-        return t.units.sideOfTheWorld.name.south;
+        return tr.units.sideOfTheWorld.name.south;
       case SideOfTheWorld.southWest:
-        return t.units.sideOfTheWorld.name.southWest;
+        return tr.units.sideOfTheWorld.name.southWest;
       case SideOfTheWorld.west:
-        return t.units.sideOfTheWorld.name.west;
+        return tr.units.sideOfTheWorld.name.west;
       case SideOfTheWorld.northWest:
-        return t.units.sideOfTheWorld.name.northWest;
-      default:
-        return '';
+        return tr.units.sideOfTheWorld.name.northWest;
     }
   }
 }
