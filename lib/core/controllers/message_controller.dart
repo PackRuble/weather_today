330287a7d45e1d531747ad2868b506aacfc49399
Ruble <pack.ruble@gmail.com>
2022-09-25 14:28:44 +0300

    add - snack and toast in app

---
+++ lib/core/controllers/message_controller.dart
@@ -0,0 +1,136 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:weather_today/core/controllers/localization_controller.dart';
+import 'package:weather_today/core/models/toasts_model.dart';
+import 'package:weather_today/utils/routes/routes.gr.dart';
+
+import 'global_key.dart';
+
+/// Здесь находится центр вызова снэков.
+class SnackController extends ChangeNotifier {
+  SnackController();
+
+  MessageSnack? snack;
+
+  void showSnack(MessageSnack snack) {
+    this.snack = snack;
+    notifyListeners();
+  }
+}
+
+/// Центр по управлению высплывающими тостами!
+class ToastController extends ChangeNotifier {
+  MessageToast? toast;
+
+  void showToast(MessageToast toast) {
+    this.toast = toast;
+    notifyListeners();
+  }
+}
+
+/// Реализует возможность показывать сообщения (Snackbar or Toast).
+class MessageController {
+  MessageController(this._ref);
+
+  final Ref _ref;
+
+  Reader get _reader => _ref.read;
+
+  static final pr =
+      Provider<MessageController>((ref) => MessageController(ref));
+
+  /// Пассивное уведомление. Не интерактивный.
+  static final toasts = ChangeNotifierProvider<ToastController>(
+      (ref) => ToastController(),
+      name: '$ToastController');
+
+  /// Активное уведомление. Интерактивный, позволяет выбрать действия.
+  static final snacks = ChangeNotifierProvider<SnackController>(
+      (ref) => SnackController(),
+      name: '$SnackController');
+
+  /// показать Toast.
+  void _showToast(MessageToast toast) => _reader(toasts).showToast(toast);
+
+  /// показать Snack.
+  void _showSnack(MessageSnack snack) => _reader(snacks).showSnack(snack);
+
+  /// Ошибка сети.
+  void tSocketException() => _showToast(_StoreMessages.toastSocketException);
+
+  /// Успешная установка ApiKey-погоды.
+  void tApiKeyWeatherSetTrue() =>
+      _showToast(_StoreMessages.toastApiKeyWeatherSetTrue);
+
+  /// Установка ApiKey-погоды не удалась.
+  void tApiKeyWeatherSetFalse() =>
+      _showToast(_StoreMessages.toastApiKeyWeatherSetFalse);
+
+  /// Обновить погоду сейчас не разрешено.
+  void sUpdateWeatherFail() =>
+      _showSnack(_StoreMessages.snackUpdateWeatherFail);
+
+  /// Апи ключ для сервиса OWM действителен.
+  void tCheckApikeyOWMSuccess() =>
+      _showToast(_StoreMessages.toastApikeyOWMSuccess);
+
+  /// Апи ключ для сервиса OWM НЕдействителен.
+  void tCheckApikeyOWMFail() => _showToast(_StoreMessages.toastApikeyOWMFail);
+}
+
+class _StoreMessages {
+  /// Уведомление об отсутствии интернета.
+  ///
+  /// Возникает если:
+  /// - Нет соединения с сервером погоды;
+  /// - Нет соединения с интернетом.
+  static final MessageToast toastSocketException = MessageToast(
+    message: tr.dialogs.messages.socketException,
+    toastTime: Toast.LENGTH_LONG,
+    gravity: ToastGravity.TOP,
+  );
+
+  /// Уведомление об успехе установки apiKey weather.
+  ///
+  static final MessageToast toastApiKeyWeatherSetTrue = MessageToast(
+    message: tr.dialogs.messages.apiKeyWeatherSetTrue,
+    toastTime: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+  );
+
+  /// Уведомление об провале установки apiKey weather.
+  ///
+  static final MessageToast toastApiKeyWeatherSetFalse = MessageToast(
+    message: tr.dialogs.messages.apiKeyWeatherSetFalse,
+    toastTime: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+  );
+
+  /// Уведомление об успешной проверке api ключа погоды.
+  ///
+  static final MessageToast toastApikeyOWMSuccess = MessageToast(
+    message: tr.dialogs.messages.apiKeyOWMVerificationSuccess,
+    toastTime: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+  );
+
+  /// Уведомление об провальной проверке api ключа погоды.
+  ///
+  static final MessageToast toastApikeyOWMFail = MessageToast(
+    message: tr.dialogs.messages.apiKeyOWMVerificationFail,
+    toastTime: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+  );
+
+  /// Уведомление о неудачном обновлении погоды.
+  ///
+  static MessageSnack snackUpdateWeatherFail = MessageSnack(
+    message: tr.dialogs.messages.weatherUpdateFail,
+    action: MapEntry(tr.dialogs.buttons.know, () {
+      return AutoRouter.of(materialKeyProvider.currentContext!)
+          .push(const UserApiRoute());
+    }),
+  );
+}
