64292207f4f8bb642894f28d35fff73f42133056
Ruble <pack.ruble@gmail.com>
2022-11-30 15:05:11 +0300

    fix: interaction with ui on desktop

--- lib/ui/shared/wrapper_page.dart
+++ lib/ui/shared/wrapper_page.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:weather_today/core/services/app_theme_service/controller/app_theme_controller.dart';
@@ -5,7 +8,8 @@ import 'package:weather_today/core/services/app_theme_service/controller/app_the
 /// Обертка позволяет сделать много полезной работы за нас.
 /// В данный момент:
 /// - следит за фокусом;
-/// - следит за размером текста.
+/// - следит за размером текста;
+/// - помогает настроить взаимодействие с приложением согласно устройства;
 class WrapperPage extends ConsumerWidget {
   const WrapperPage({required this.child});
 
@@ -15,12 +19,32 @@ class WrapperPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final double textScaleFactor = ref.watch(AppTheme.textScaleFactor);
 
+    Widget _child;
+
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+      case TargetPlatform.iOS:
+        _child = child;
+        break;
+      // ignore: no_default_cases
+      default:
+        _child = ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(
+            dragDevices: {
+              PointerDeviceKind.touch,
+              PointerDeviceKind.mouse,
+            },
+          ),
+          child: child,
+        );
+    }
+
     // Применяем масштабирование текста во всем приложении
     return MediaQuery(
       data: MediaQuery.of(context).copyWith(textScaleFactor: textScaleFactor),
       child: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
-        child: child,
+        child: _child,
       ),
     );
   }
