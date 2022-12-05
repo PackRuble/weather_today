4f99c02afb4279a3e620db701414369aed603f99
Ruble <pack.ruble@gmail.com>
2022-12-05 11:19:47 +0300

    fix: appbar

--- lib/ui/shared/appbar_widget.dart
+++ lib/ui/shared/appbar_widget.dart
@@ -1,7 +1,5 @@
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:weather_today/core/services/app_theme_service/controller/app_theme_controller.dart';
 
 import 'theme_button.dart';
 
@@ -24,35 +22,19 @@ class RAppBar extends ConsumerWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final theme = Theme.of(context);
-
-    // bug нельзя даже изменить цвет иконок, когда [useMaterial3=true], ждем исправления:
+    // bug нельзя даже изменить цвет иконок, ждем исправления:
     // https://github.com/flutter/flutter/issues/110878
-
-    // А пока что [useMaterial3=false] для appbar
-    final bool isDark = Brightness.dark == theme.brightness;
-
-    final FlexColorScheme nowFlexTheme =
-        ref.watch(AppTheme.usingThemeNow(isDark)).copyWith(
-              useMaterial3: false,
-            );
-
-    return Theme(
-      data: nowFlexTheme.toTheme,
-      child: AppBar(
-        titleSpacing: 0.0,
-        backgroundColor:
-            theme.appBarTheme.backgroundColor, // remove, when fix _bug
-        // actionsIconTheme: IconThemeData(color: Colors.blue),
-        title: Text(
-          title,
-          overflow: TextOverflow.fade,
-        ),
-        actions: [
-          if (addThemeButton) const ChangerThemeButton(),
-          if (actions != null) ...actions!,
-        ],
+    return AppBar(
+      titleSpacing: 0.0,
+      // actionsIconTheme: IconThemeData(color: Colors.blue),
+      title: Text(
+        title,
+        overflow: TextOverflow.fade,
       ),
+      actions: [
+        if (addThemeButton) const ChangerThemeButton(),
+        if (actions != null) ...actions!,
+      ],
     );
   }
 }
