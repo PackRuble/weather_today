b4a3a9cbbfc048579631ccf4e41e3c56fee1e7b7
Ruble <pack.ruble@gmail.com>
2023-09-18 12:59:33 +0300

    feat: slide effect when closing a window using gestures

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,4 +1,7 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:dismissible_page/dismissible_page.dart';
+import 'package:flutter/material.dart';
+
 import 'routes.gr.dart';
 
 @AutoRouterConfig(
@@ -8,15 +11,50 @@ class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(page: HomeRoute.page, initial: true),
-        AutoRoute(page: SavedPlacesRoute.page),
-        AutoRoute(page: WeatherLanguageRoute.page),
-        AutoRoute(page: UserApiRoute.page),
-        AutoRoute(page: CountryFlagsRoute.page),
-        AutoRoute(page: ThemeRoute.page),
-        AutoRoute(page: VisualDesignRoute.page),
-        AutoRoute(page: GratitudeRoute.page),
-        AutoRoute(page: LogsRoute.page),
-        AutoRoute(page: SystemSettingsRoute.page),
-        AutoRoute(page: TermsAppRoute.page),
+        DismissibleRoute(page: SavedPlacesRoute.page),
+        DismissibleRoute(page: WeatherLanguageRoute.page),
+        DismissibleRoute(page: UserApiRoute.page),
+        DismissibleRoute(page: CountryFlagsRoute.page),
+        DismissibleRoute(page: ThemeRoute.page),
+        DismissibleRoute(page: VisualDesignRoute.page),
+        DismissibleRoute(page: GratitudeRoute.page),
+        DismissibleRoute(page: LogsRoute.page),
+        DismissibleRoute(page: SystemSettingsRoute.page),
+        DismissibleRoute(page: TermsAppRoute.page),
       ];
+}
+
+class DismissibleRoute extends CustomRoute {
+  DismissibleRoute({
+    required super.page,
+  }) : super(customRouteBuilder: _dismissiblePageRouteBuilder);
+
+  static TransparentRoute<T> _dismissiblePageRouteBuilder<T>(
+    BuildContext context,
+    Widget child,
+    AutoRoutePage<T> page,
+  ) {
+    return TransparentRoute<T>(
+      builder: (_) => DismissiblePage(
+        backgroundColor: Colors.black,
+        minRadius: 0.0,
+        maxRadius: 24.0,
+        dragSensitivity: 0.7,
+        startingOpacity: 0.5,
+        onDismissed: () => Navigator.of(context).pop(),
+        direction: DismissiblePageDismissDirection.startToEnd,
+        dismissThresholds: const {
+          DismissiblePageDismissDirection.startToEnd: 0.4
+        },
+        reverseDuration: const Duration(milliseconds: 100),
+        maxTransformValue: 0.5,
+        child: child,
+      ),
+      fullscreenDialog: false,
+      backgroundColor: Colors.transparent,
+      transitionDuration: const Duration(milliseconds: 250),
+      reverseTransitionDuration: const Duration(milliseconds: 250),
+      settings: page,
+    );
+  }
 }
