bf125cdd2bf3041711bc42e9c5de5056c3c1b0ff
Ruble <pack.ruble@gmail.com>
2023-10-23 11:39:02 +0300

    fix: migration tool doesn't need build number

--- lib/domain/init_app_service.dart
+++ lib/domain/init_app_service.dart
@@ -1,6 +1,5 @@
 import 'package:cardoteka/cardoteka.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:weather_today/application/const/app_info.dart';
 import 'package:weather_today/domain/controllers/app_theme/controller/app_theme_controller.dart';
 import 'package:weather_today/domain/controllers/logger_controller.dart';
 import 'package:weather_today/domain/controllers/owm_controller.dart';
@@ -43,7 +42,6 @@ class ServiceInit {
     await MigrationTool(
       localStorageOld: _container.read(dbService),
       settingsStorageNew: _container.read(SettingsStorage.instance),
-      buildNumber: await AppInfo.buildNumber,
     ).migrate();
   }
 
--- lib/domain/services/migration_tool/sp_to_cardoteka.dart
+++ lib/domain/services/migration_tool/sp_to_cardoteka.dart
@@ -3,52 +3,55 @@ import 'package:weather_today/domain/services/cardoteka/settings_storage.dart';
 import 'package:weather_today/domain/services/local_storage/interface/data_base.dart';
 import 'package:weather_today/domain/services/local_storage/key_store.dart';
 
+/// Used as an annotation to associate migrations and build number.
+class _Migrate {
+  const _Migrate(this.buildNumber);
+
+  /// A number indicating the assembly in which the migration was first performed.
+  final String buildNumber;
+}
+
 class MigrationTool {
   const MigrationTool({
     required this.localStorageOld,
     required this.settingsStorageNew,
-    required this.buildNumber,
   });
 
   final DataBase localStorageOld;
   final SettingsStorage settingsStorageNew;
-  final int buildNumber;
-
-  static const int buildNumberMigration18 = 18;
 
   Future<void> migrate() async {
-    if (buildNumber >= buildNumberMigration18) {
-      await Future.wait([_visualDesignMigrate()]);
-    }
+    await Future.wait([_visualDesignMigrate()]);
   }
 
+  @_Migrate('18')
   Future<void> _visualDesignMigrate() async {
     const oldKey = DbStore.visualDesignApp;
 
-    if (await localStorageOld.contains(oldKey)) {
-      final visualDesignAppStored = await localStorageOld.load(
-        oldKey,
-        DbStore.visualDesignAppDefault,
-      );
+    if (!(await localStorageOld.contains(oldKey))) return;
 
-      final design = switch (visualDesignAppStored) {
-        0 => AppVisualDesign.byRuble,
-        1 => AppVisualDesign.byTolskaya,
-        _ => AppVisualDesign.byRuble,
-      };
+    final visualDesignAppStored = await localStorageOld.load(
+      oldKey,
+      DbStore.visualDesignAppDefault,
+    );
 
-      final pagesNew = [
-        DesignPage(page: WeatherPage.hourly, design: design),
-        DesignPage(page: WeatherPage.currently, design: design),
-        DesignPage(page: WeatherPage.daily, design: design),
-      ];
+    final design = switch (visualDesignAppStored) {
+      0 => AppVisualDesign.byRuble,
+      1 => AppVisualDesign.byTolskaya,
+      _ => AppVisualDesign.byRuble,
+    };
 
-      await settingsStorageNew.set<List<DesignPage>>(
-        SettingsCards.designPages,
-        pagesNew,
-      );
+    final pagesNew = [
+      DesignPage(page: WeatherPage.hourly, design: design),
+      DesignPage(page: WeatherPage.currently, design: design),
+      DesignPage(page: WeatherPage.daily, design: design),
+    ];
 
-      await localStorageOld.clearKey(oldKey);
-    }
+    await settingsStorageNew.set<List<DesignPage>>(
+      SettingsCards.designPages,
+      pagesNew,
+    );
+
+    await localStorageOld.clearKey(oldKey);
   }
 }
