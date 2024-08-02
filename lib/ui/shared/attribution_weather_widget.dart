16cf65860efbad91304e9e4d86f340b838a31bbc
Ruble <pack.ruble@gmail.com>
2024-08-02 13:03:31 +0300

    add: use weather attribution according to the selected service

--- lib/ui/shared/attribution_weather_widget.dart
+++ lib/ui/shared/attribution_weather_widget.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:weather_today/application/const/app_colors.dart';
+import 'package:weather_today/domain/controllers/weather_provider_nr.dart';
+import 'package:weather_today/domain/weather/models.dart';
 
-/// Виджет-плашка поставщика погоды.
-class AttributionWeatherWidget extends StatelessWidget {
+extension on WeatherProvider {
+  Uri get website => switch (this) {
+        WeatherProvider.openWeatherMap =>
+          Uri.parse('https://openweathermap.org/'),
+        WeatherProvider.openMeteo => Uri.parse('https://open-meteo.com/'),
+      };
+
+  String logoAsset([bool forDark = false]) => switch (this) {
+        WeatherProvider.openWeatherMap =>
+          'assets/images/attribution/owm/openweather_${forDark ? 'negative' : 'master'}_logo.png',
+        WeatherProvider.openMeteo =>
+          'assets/images/attribution/open-meteo/openmeteo_logo.png',
+      };
+
+  String get nameService => switch (this) {
+        WeatherProvider.openWeatherMap => 'OpenWeather',
+        WeatherProvider.openMeteo => 'Open-Meteo',
+      };
+}
+
+/// Weather provider attribution widget.
+class AttributionWeatherWidget extends ConsumerWidget {
   const AttributionWeatherWidget({
     this.padding = const EdgeInsets.all(5.0),
     this.alignment = Alignment.centerRight,
@@ -13,15 +36,13 @@ class AttributionWeatherWidget extends StatelessWidget {
   final EdgeInsets padding;
 
   @override
-  Widget build(BuildContext context) {
-    final isLight = AppColors.of(context).isLight;
-
-    const String nameService = 'Weather data provided by ';
-    final Uri urlService = Uri.parse('https://openweathermap.org/');
-
+  Widget build(BuildContext context, WidgetRef ref) {
     final textStyle =
         Theme.of(context).textTheme.bodySmall?.copyWith(fontSize: 10);
 
+    final isLight = AppColors.of(context).isLight;
+
+    final weatherProvider = ref.watch(WeatherProviderNR.i);
     return Align(
       alignment: alignment,
       child: Padding(
@@ -30,24 +51,29 @@ class AttributionWeatherWidget extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Image.asset(
-              isLight
-                  ? 'assets/images/attribution/owm/openweather_master_logo.png'
-                  : 'assets/images/attribution/owm/openweather_negative_logo.png',
-              width: 40,
-            ),
             Flexible(
               child: Text.rich(
                 textAlign: TextAlign.center,
-                const TextSpan(text: nameService),
+                const TextSpan(text: 'Weather data provided by '),
                 style: textStyle,
               ),
             ),
+            Image.asset(
+              weatherProvider.logoAsset(!isLight),
+              height: 20,
+            ),
+            const SizedBox(width: 4),
             InkWell(
-              onTap: () async =>
-                  launchUrl(urlService, mode: LaunchMode.externalApplication),
+              onTap: () async {
+                final url = weatherProvider.website;
+
+                await launchUrl(
+                  url,
+                  mode: LaunchMode.externalApplication,
+                );
+              },
               child: Text(
-                'OpenWeather',
+                weatherProvider.nameService,
                 style: textStyle?.copyWith(
                   decoration: TextDecoration.underline,
                   decorationColor: Colors.blue,
