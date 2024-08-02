f6a0eb95835b7ccd384b9f3d8ab05559549ad76f
Ruble <pack.ruble@gmail.com>
2024-08-02 15:09:27 +0300

    upd: latest dependencies

--- lib/ui/shared/markdown_future_builder.dart
+++ lib/ui/shared/markdown_future_builder.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_markdown_selectionarea/flutter_markdown_selectionarea.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MarkdownFutureBuilder extends HookWidget {
@@ -29,7 +29,6 @@ class MarkdownFutureBuilder extends HookWidget {
                   mode: LaunchMode.externalApplication,
                 );
               },
-              // ignore: deprecated_member_use
               selectable: false,
               data: snapshot.data!,
             ),
