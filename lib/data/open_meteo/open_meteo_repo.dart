b24bccc6cf2c802c3a5d923e23773439a9c3b3bb
Ruble <pack.ruble@gmail.com>
2024-08-03 16:08:46 +0300

    add: `OpenMeteoApiException`

--- lib/data/open_meteo/open_meteo_repo.dart
+++ lib/data/open_meteo/open_meteo_repo.dart
@@ -1,12 +1,28 @@
 import 'dart:convert' show jsonDecode;
 
 import 'package:http/http.dart' as http;
-import 'package:weather_pack/weather_pack.dart';
 import 'package:weather_today/domain/models/place/place_model.dart';
 import 'package:weather_today/utils/logger/_observers/global_logger.dart';
 
 import 'models/enums.dart';
 import 'models/models.dart';
+
+class OpenMeteoApiException implements Exception {
+  const OpenMeteoApiException(this.code, this.message);
+
+  const OpenMeteoApiException.error(Object error, StackTrace stackTrace)
+      : message = '$error\n$stackTrace',
+        code = 0;
+
+  /// Message about error.
+  final String message;
+
+  /// Code api error.
+  final int code;
+
+  @override
+  String toString() => '$this(code: $code, message: $message)';
+}
 
 class OpenMeteoRepo {
   const OpenMeteoRepo();
@@ -30,24 +46,21 @@ class OpenMeteoRepo {
 
       final http.Response response = await http.get(uri);
 
+      final json = jsonDecode(response.body) as Map<String, dynamic>;
+
       switch (response.statusCode) {
         case 200:
           break;
         default:
-          // todo(03.08.2024): create OpenMeteoException
-          //  example:
-          // {
-          //   "error": true,
-          //   "reason": "Cannot initialize WeatherVariable from invalid String value tempeture_2m for key hourly"
-          // }
-          throw OwmApiException(response.statusCode, response.body);
+          throw OpenMeteoApiException(
+            response.statusCode,
+            json["reason"].toString(),
+          );
       }
-
-      final json = jsonDecode(response.body) as Map<String, dynamic>;
 
       result = ForecastOpenMeteoResponse.fromJson(json);
     } catch (error, stackTrace) {
-      throw OwmApiException.error(error, stackTrace);
+      throw OpenMeteoApiException.error(error, stackTrace);
     }
 
     return result;
