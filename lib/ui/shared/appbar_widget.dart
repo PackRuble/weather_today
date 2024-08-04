8aa4f1bb24eab1b38ceaf59d7050822c64b176cc
Ruble <pack.ruble@gmail.com>
2024-08-04 10:30:43 +0300

    upd: increase leading width in appbar

--- lib/ui/shared/appbar_widget.dart
+++ lib/ui/shared/appbar_widget.dart
@@ -38,13 +38,13 @@ class AppBarCustom extends StatelessWidget implements PreferredSizeWidget {
 
     return AppBar(
       titleSpacing: 0.0,
-      leadingWidth: 40,
+      leadingWidth: 48.0,
       title: Text(
         title,
         overflow: TextOverflow.fade,
       ),
       titleTextStyle: theme.textTheme.headlineSmall?.copyWith(
-        fontSize: 20,
+        fontSize: 18,
         color: theme.appBarTheme.foregroundColor,
       ),
       leading: IconButton(
