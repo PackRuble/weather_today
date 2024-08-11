d533631e4e0c63246c7538cbe9674714b28ccc1d
Ruble <pack.ruble@gmail.com>
2024-08-11 14:27:17 +0300

    upd: `AppLinks`

--- lib/application/const/app_links.dart
+++ lib/application/const/app_links.dart
@@ -1,18 +1,20 @@
 class AppLinks {
   const AppLinks._();
 
+  static const _weatherDoc = 'PackRuble/public_doc/main/weather_today_doc';
+
   static final privacyPolicyUrl = Uri.https(
     'raw.githubusercontent.com',
-    'PackRuble/public_doc/main/weather_today_doc/privacy_policy.md',
+    '$_weatherDoc/privacy_policy.md',
   );
 
   static final termsOfUseAppUrl = Uri.https(
     'raw.githubusercontent.com',
-    'PackRuble/public_doc/main/weather_today_doc/terms_of_use_app.md',
+    '$_weatherDoc/terms_of_use_app.md',
   );
 
   static final termsAndConditionsUrl = Uri.https(
-    'github.com',
-    'PackRuble/public_doc/main/weather_today_doc/terms&conditions.md?raw=true',
+    'raw.githubusercontent.com',
+    '$_weatherDoc/terms&conditions.md',
   );
 }
