2ad02ccd5f4a4d0dec7006bf67aa1ec8548f4f85
Ruble <pack.ruble@gmail.com>
2024-08-11 14:27:00 +0300

    add: `ExpPanel` and `RotatedIcon`, `ScrollOnExpand`

--- lib/ui/shared/expandable_custom.dart
+++ lib/ui/shared/expandable_custom.dart
@@ -1,89 +1,197 @@
-import 'package:expandable/expandable.dart';
-import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-/// Use [ExpandableInexpensive] for optimization.
-class ExpandablePanelCustom extends StatelessWidget {
-  const ExpandablePanelCustom({
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:weather_today/extension/value_notifier_x.dart';
+
+/// Optimized expanded panel.
+class ExpPanel extends HookWidget {
+  const ExpPanel({
+    super.key,
     required this.header,
+    this.expandIcon,
+    this.iconPadding = const EdgeInsets.only(right: 8.0),
     required this.expanded,
-    this.expandableTheme,
+    this.scrollOnExpand = true,
+    this.collapsed,
+    this.onExpand,
+    this.animationDuration = const Duration(milliseconds: 300),
+    this.initialExpanded = false,
+    this.hasIcon = true,
   });
 
-  final Widget? header;
+  final Widget header;
+  final Widget? expandIcon;
+
+  /// Ignore when using [expandIcon]
+  final EdgeInsets iconPadding;
 
   final Widget expanded;
+  final Widget? collapsed;
 
-  final ExpandableThemeData? expandableTheme;
+  final Duration animationDuration;
+
+  final void Function(bool isExpanding)? onExpand;
+
+  final bool initialExpanded;
+  final bool scrollOnExpand;
+  final bool hasIcon;
 
   @override
   Widget build(BuildContext context) {
-    return ExpandableNotifier(
-      child: ScrollOnExpand(
-        child: ExpandablePanel(
-          theme: const ExpandableThemeData(
-            headerAlignment: ExpandablePanelHeaderAlignment.center,
-            tapHeaderToExpand: true,
-            tapBodyToExpand: false,
-            tapBodyToCollapse: false,
-            hasIcon: false,
-          ),
-          header: header,
-          builder: (_, collapsed, expanded) => ExpandableInexpensive(
-            collapsed: collapsed,
-            expanded: expanded,
-          ),
-          collapsed: const SizedBox.shrink(),
-          expanded: expanded,
-        ),
+    final expCR = useValueNotifier(initialExpanded, [initialExpanded]);
+    final isExpanded = useValueListenable(expCR);
+
+    useEffect(() {
+      // ignore: prefer_function_declarations_over_variables
+      final remover = expCR.addListenerV((value) => onExpand?.call(value));
+      return remover;
+    });
+
+    Widget child = isExpanded ? expanded : collapsed ?? const SizedBox.shrink();
+
+    child = AnimatedSwitcher(
+      duration: animationDuration,
+      transitionBuilder: sizeTransitionBuilder,
+      child: SizedBox(
+        key: ValueKey<bool>(isExpanded),
+        child: child,
       ),
+    );
+
+    return ScrollOnExpand(
+      isExpanded: isExpanded,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          InkWell(
+            onTap: () => expCR.value = !isExpanded,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Flexible(child: header),
+                expandIcon ??
+                    Padding(
+                      padding: iconPadding,
+                      child: RotatedIcon(
+                        isExpanded: isExpanded,
+                        icon: hasIcon
+                            ? const Icon(Icons.expand_more)
+                            : const SizedBox.shrink(),
+                      ),
+                    ),
+              ],
+            ),
+          ),
+          Flexible(
+            child: child,
+          )
+        ],
+      ),
+    );
+  }
+
+  static Widget sizeTransitionBuilder(
+    Widget child,
+    Animation<double> animation,
+  ) =>
+      SizeTransition(
+        axis: Axis.vertical,
+        sizeFactor: animation,
+        axisAlignment: -1,
+        child: child,
+      );
+}
+
+class RotatedIcon extends HookWidget {
+  const RotatedIcon({
+    super.key,
+    this.icon,
+    this.collapsedIcon,
+    this.expandedIcon,
+    this.iconRotationAngle = -math.pi,
+    this.onTap,
+    required this.isExpanded,
+  });
+
+  /// Has priority over [collapsedIcon] and [expandedIcon]
+  final Widget? icon;
+  final Widget? collapsedIcon;
+  final Widget? expandedIcon;
+  final double iconRotationAngle;
+  final bool isExpanded;
+  final void Function(bool isExpanding)? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final animCR = useAnimationController(
+      initialValue: isExpanded ? 1.0 : 0.0,
+      duration: Durations.medium2,
+      reverseDuration: Durations.medium2,
+    );
+
+    useEffect(() {
+      isExpanded ? animCR.forward() : animCR.reverse();
+      return null;
+    }, [isExpanded]);
+
+    return AnimatedBuilder(
+      animation: animCR,
+      builder: (context, child) {
+        late final isExpanding = animCR.value > 0.5;
+        return Transform.rotate(
+          angle: iconRotationAngle * animCR.value,
+          // (showSecondIcon ? -(1.0 - animCR.value) : animCR.value),
+          child: icon ?? (isExpanding ? collapsedIcon : expandedIcon),
+        );
+      },
     );
   }
 }
 
-/// Optimized builder for use instead of [Expandable].
-/// Draws only when revealed.
-class ExpandableInexpensive extends StatelessWidget {
-  const ExpandableInexpensive({
+class ScrollOnExpand extends HookWidget {
+  const ScrollOnExpand({
     super.key,
-    required this.collapsed,
-    required this.expanded,
+    this.scrollOnExpand = true,
+    this.scrollOnCollapse = true,
+    this.duration = const Duration(milliseconds: 300),
+    required this.child,
+    required this.isExpanded,
   });
 
-  final Widget collapsed;
-  final Widget expanded;
+  final Widget child;
+
+  final Duration duration;
+
+  final bool isExpanded;
+
+  final bool scrollOnExpand;
+
+  final bool scrollOnCollapse;
 
   @override
   Widget build(BuildContext context) {
-    final expCR = ExpandableController.of(context)!;
-    final expTheme = ExpandableThemeData.of(context);
+    final scroll = useRef<bool>(false);
 
-    return ClipRect(
-      child: AnimatedSize(
-        duration: expTheme.animationDuration!,
-        curve: expTheme.sizeCurve!,
-        child: AnimatedSwitcher(
-          duration: expTheme.animationDuration!,
-          switchInCurve: Interval(
-            expTheme.collapsedFadeStart,
-            expTheme.collapsedFadeEnd,
-            curve: expTheme.fadeCurve!,
-          ),
-          switchOutCurve: Interval(
-            expTheme.expandedFadeStart,
-            expTheme.expandedFadeEnd,
-            curve: expTheme.fadeCurve!,
-          ),
-          layoutBuilder: (currentChild, previousChildren) => Stack(
-            clipBehavior: Clip.none,
-            alignment: expTheme.alignment!,
-            children: <Widget>[
-              ...previousChildren,
-              if (currentChild != null) currentChild,
-            ],
-          ),
-          child: expCR.expanded ? expanded : collapsed,
-        ),
-      ),
-    );
+    useEffect(() {
+      if (scroll.value) {
+        Future.delayed(
+          duration,
+          () {
+            if (!context.mounted) return;
+
+            if (scrollOnExpand || scrollOnExpand) {
+              context.findRenderObject()?.showOnScreen(duration: duration);
+            }
+          },
+        );
+      }
+
+      scroll.value = true;
+
+      return null;
+    }, [isExpanded]);
+
+    return child;
   }
 }
