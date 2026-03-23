5cbfe611342a1ba8a731a8e4310473b8f8b458d1
Ruble <pack.ruble@gmail.com>
2026-03-23 18:10:23 +0300

    upd!: regenerate project with Flutter 3.41.5 | Dart 3.11.3

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,83 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "com.Ruble.Pack.weather_today"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.Ruble.Pack.weather_today"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    flavorDimensions += "weather_today"
+
+    productFlavors {
+        create("dev_debug") {
+            dimension = "weather_today"
+            applicationIdSuffix = ".dev_debug"
+            resValue("string", "app_name", "Weather Today [dev_debug]")
+            versionNameSuffix = ".dev_debug"
+        }
+        create("dev") {
+            dimension = "weather_today"
+            applicationIdSuffix = ".dev"
+            resValue("string", "app_name", "Weather Today [DEV]")
+            versionNameSuffix = ".dev"
+        }
+        create("prod") {
+            dimension = "weather_today"
+            resValue("string", "app_name", "Weather Today")
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
